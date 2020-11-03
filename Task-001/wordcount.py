33e40ff10cc7ed37841cc5888d88a87e5568ddb6
NathanMinter <nathan.minter@appexperts.me.uk>
2020-11-03 11:40:04 +0000

    Task-001

---
+++ Task-001/wordcount.py
@@ -0,0 +1,13 @@
+import string
+
+with open("Shakespeare.txt", "r") as f:
+	words_all = f.read().replace("\n", "").lower()
+
+for ch in string.punctuation:
+	words_all = words_all.replace(ch, " ")
+
+words_set = words_all.split(" ")
+
+for x in words_set:
+	n = words_all.count(x)
+	print("{}: {}".format(x,n))
---
+++ Task-001/wordcount.scala
@@ -0,0 +1,13 @@
+import scala.io.Source
+
+def wordCount (substr: String): Int =
+  wordsAll.sliding(substr.length).count(window => window == substr)
+
+val wordsAll = Source.fromFile("Shakespeare.txt").getLines.mkString.replace("\n", "").replaceAll("""[\p{Punct}]""", " ").toLowerCase()
+
+val wordsSet: Set[String] = wordsAll.split(" ").toSet
+
+for (x <- wordsSet) {
+  var n = wordCount(x)
+  println(s"$x: $n")
+}
