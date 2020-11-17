764e28fc9ee5b07bb248e48d9e58471ce82927b9
NathanMinter <nathan.minter@appexperts.me.uk>
2020-11-17 16:51:18 +0000

    In-class task

--- Task-010/slack_kafka.py
+++ Task-010/slack_kafka.py
@@ -4,15 +4,55 @@ import time
 import re
 import kafka as kf
 import slack
+import requests
+from slack import RTMClient
 from slack.errors import SlackApiError
 
 ## Set auth tokens
 with open('/home/n/opt/MindBender_BD/Misc/keys') as keys:
     slack_keys = json.load(keys)
     bot_access_token = slack_keys["slack"]["bot_access_token"]
+    signing_secret = slack_keys["slack"]["signing_secret"]
 
+## Initialise authentications
 client = slack.WebClient(token=bot_access_token)
 
+@RTMClient.run_on(event="message")
+def amusebot(**payload):
+    ## This function triggers when someone sends a message on the slack
+    data = payload["data"]
+    web_client = payload["web_client"]
+    bot_id = data.get("bot_id", "")
+
+    # If a message is not sent by the bot
+    if bot_id == "":
+        channel_id = data["channel"]
+
+        # Extracting message sent by the user on the slack
+        text = data.get("text", "")
+        text = text.split(">")[-1].strip()
+
+        response = ""
+        if "help" in text.lower():
+            user = data.get("user", "")
+            response = f"Hi <@{user}>! I am AmuseBot :)"
+        else:
+            activity_json_response = requests.get("http://www.boredapi.com/api/activity/").json()
+            activity = activity_json_response['activity']
+            response = str(activity)
+        
+        # Sending message back to slack
+        web_client.chat_postMessage(channel=channel_id, text=response)
+
+try:
+    rtm_client = RTMClient(token=bot_access_token)
+    print("Bot is up and running!")
+    rtm_client.start()
+except Exception as err:
+    print(err)
+
+
+"""
 ## Post message with bot.
 try:
     response = client.chat_postMessage(
@@ -24,6 +64,7 @@ except SlackApiError as e:
     assert e.response["ok"] is False
     assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
     print(f"Got an error: {e.response['error']}")
+"""
 
 """
 ## Kafka stream listener (topic created in terminal)
@@ -32,3 +73,8 @@ kafka = kf.KafkaClient("localhost:9099")
 producer = kf.SimpleProducer(kafka)
 #listen = StdOutListener()
 """
+
+
+
+x = requests.get("https://app.slack.com/client/T01EX004L10/C01FLQUGUE4")
+print(x)
---
+++ Task-011/spark_submit_class.py
@@ -0,0 +1,31 @@
+"""
+import sys
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+
+sc = SparkContext("spark://local[*]", "ShakespeareStream")
+ssc = StreamingContext(sc, 3)
+lines = ssc.textFileStream('/home/n/opt/MindBender_BD/Python/Shakespeare')
+counts = lines.flatMap(lambda line: line.split(" ")) \
+    .map(lambda x: (x, 1)) \
+    .reduceByKey(lambda a, b: a + b)
+counts.pprint()
+
+ssc.start()
+ssc.awaitTermination()
+"""
+
+import sys
+from pyspark import SparkContext, SparkConf
+
+## Create Spark context
+sc = SparkContext("local","Shakespeare")
+
+## Read data from text file and split each line into words
+words = sc.textFile("/home/n/opt/MindBender_BD/Task-001/Shakespeare.txt").flatMap(lambda line: line.split(" "))
+
+## Count the occurrence of each word
+wordcount = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b)
+
+## Save to output
+wordcount.saveAsTextFile("/home/n/opt/MindBender_BD/Task-011/")
