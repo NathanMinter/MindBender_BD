5d6490874df00ad2523e9377e33bae065e55ee19
NathanMinter <nathan.minter@appexperts.me.uk>
2020-11-17 12:20:57 +0000

    Slack bot created and authorized

--- Task-010/slack_kafka.py
+++ Task-010/slack_kafka.py
@@ -1,26 +1,22 @@
 import os
+import json
+import time
+import re
 import kafka as kf
 import slack
 from slack.errors import SlackApiError
 
-"""
-## 
-class StdOutListener(StreamListener):
-    def on_data(self, data):
-        producer.send_messages(topic, data.encode('utf-8'))
-        print("Tweet Sent")
-        return True
-    def on_error(self, status):
-        print(status)
-"""
+## Set auth tokens
+with open('/home/n/opt/MindBender_BD/Misc/keys') as keys:
+    slack_keys = json.load(keys)
+    bot_access_token = slack_keys["slack"]["bot_access_token"]
 
-slack_api_token = ""
+client = slack.WebClient(token=bot_access_token)
 
-client = slack.WebClient(token=os.environ['slack_api_token'])
-
+## Post message with bot.
 try:
     response = client.chat_postMessage(
-        channel='#random',
+        channel='#scrum-bot',
         text="Hello world!")
     assert response["message"]["text"] == "Hello world!"
 except SlackApiError as e:
@@ -29,12 +25,10 @@ except SlackApiError as e:
     assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
     print(f"Got an error: {e.response['error']}")
 
-## Set authorizations
-#auth = tw.OAuthHandler(consumer_key, consumer_secret)
-
+"""
 ## Kafka stream listener (topic created in terminal)
 topic = "slack"
 kafka = kf.KafkaClient("localhost:9099")
 producer = kf.SimpleProducer(kafka)
 #listen = StdOutListener()
-
+"""
