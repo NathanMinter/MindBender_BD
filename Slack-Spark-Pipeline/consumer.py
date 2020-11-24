5f2321deb91982be823cf17e4cd6b1f08ef7caaf
NathanMinter <nathan.minter@appexperts.me.uk>
2020-11-24 15:35:01 +0000

    Hive adjustment

--- Slack-Spark-Pipeline/consumer.py
+++ Slack-Spark-Pipeline/consumer.py
@@ -8,7 +8,7 @@ from pyspark.sql import SparkSession
 ## Create sessions & contexts
 sc = SparkContext("local[*]", "slackScrumBot")
 ssc = StreamingContext(sc, 3)
-ss = SparkSession.builder.appName(sc.appName).getOrCreate()
+ss = SparkSession.builder.appName(sc.appName).config("spark.sql.warehouse.dir", "/user/hive/warehouse").config("hive.metastore.urls", "thrift://localhost:9083").enableHiveSupport().getOrCreate()
 
 ## Set Kafka stream
 kafkaStream = KafkaUtils.createStream(ssc, "localhost:2181", "slack", {"slack": 1})
@@ -23,7 +23,7 @@ def process(rdd):
         global ss
         df = ss.createDataFrame(rdd, schema=["user", "text"])
         df.show()
-        #df.write.saveAsTable(name="slack_db.slack_tbl", format="hive", mode="append")
+        df.write.saveAsTable(name="slack_db.slack_tbl", format="hive", mode="append")
 
 ## Write parsed messages to Hive
 parsed.foreachRDD(process)
