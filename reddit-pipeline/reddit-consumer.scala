95465758759cfe23c5d7dc0e854063ceb1ed6148
NathanMinter <nathan.minter@appexperts.me.uk>
2020-12-01 16:44:02 +0000

    Scala consumer in progress

---
+++ reddit-pipeline/reddit-consumer.scala
@@ -0,0 +1,67 @@
+import consumer.kafka.{ProcessedOffsetManager, ReceiverLauncher}
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.{SparkConf, SparkContext};
+
+object LowLevelKafkaConsumer {
+
+  def main(arg: Array[String]): Unit = {
+
+    import org.apache.log4j.{Level, Logger}
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+
+    //Create SparkContext
+    val conf = new SparkConf()
+      .setMaster("spark://localhost:7077")
+      .setAppName("LowLevelKafkaConsumer")
+      .set("spark.executor.memory", "1g")
+      .set("spark.rdd.compress","true")
+      .set("spark.storage.memoryFraction", "1")
+      .set("spark.streaming.unpersist", "true")
+
+    val sc = new SparkContext(conf)
+    val ssc = new StreamingContext(sc, Seconds(10))
+
+    val topic = "reddit"
+    val zkhosts = "localhost"
+    val zkports = "2181"
+
+    //Specify number of Receivers you need.
+    val numberOfReceivers = 3
+
+    val kafkaProperties: Map[String, String] =
+	Map("zookeeper.hosts" -> zkhosts,
+        "zookeeper.port" -> zkports,
+        "kafka.topic" -> topic,
+        "zookeeper.consumer.connection" -> "localhost:2181",
+        "kafka.consumer.id" -> "kafka-consumer",
+        //optional properties
+        //"consumer.forcefromstart" -> "false",
+        //"consumer.backpressure.enabled" -> "true",
+        //"max.poll.records" -> "100",
+        //"consumer.fillfreqms" -> "1000",
+        //"consumer.num_fetch_to_buffer" -> "5")
+
+    val props = new java.util.Properties()
+    kafkaProperties foreach { case (key,value) => props.put(key, value)}
+
+    val unionStreamsm = ReceiverLauncher.launch(ssc, props, numberOfReceivers,StorageLevel.MEMORY_ONLY)
+    
+    unionStreams.foreachRDD(rdd => {
+        //Start Application Logic e.g. rdd.foreachPartition
+        println("\n\nNumber of records in this batch : " + rdd.count())
+        //End Application Logic
+
+        //Persists the Max Offset of given Kafka Partition to ZK
+        ProcessedOffsetManager.persistsPartition(rdd, props)
+
+    } )
+
+    ssc.start()
+    ssc.awaitTermination()
+
+
+  }
+
+}
--- reddit-pipeline/startReddit.sh
+++ reddit-pipeline/startReddit.sh
@@ -5,5 +5,5 @@ kafka-server-start.sh -daemon /home/n/opt/kafka-2.3.1/config/server2.properties
 kafka-server-start.sh -daemon /home/n/opt/kafka-2.3.1/config/server3.properties
 kafka-console-consumer.sh --bootstrap-server localhost:9099 --topic reddit &
 ttab 'hive --service metastore' &
-ttab 'spark-submit --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.4.4 ~/opt/MindBender_BD/reddit-pipeline/reddit-consumer.py' &
+ttab 'spark-submit --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.4.4 ~/opt/MindBender_BD/reddit-pipeline/reddit-consumer.scala' &
 ttab 'python3 /home/n/opt/MindBender_BD/reddit-pipeline/reddit-producer.py' &
