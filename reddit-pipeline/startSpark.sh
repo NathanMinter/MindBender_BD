2129b37e1ec05db9594ec13dcc19a1ddf9d9ab87
NathanMinter <nathan.minter@appexperts.me.uk>
2020-12-02 15:21:33 +0000

    Python-only reddit pipeline

--- reddit-pipeline/startSpark.sh
+++ reddit-pipeline/startSpark.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-spark-submit --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.3.2 ~/opt/MindBender_BD/reddit-pipeline/reddit-consumer.scala
+spark-submit --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.3.2 ~/opt/MindBender_BD/reddit-pipeline/reddit-consumer.py
