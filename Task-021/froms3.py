fa4eb72df26df5f0a224de9951a27916eb07a3c8
NathanMinter <nathan.minter@appexperts.me.uk>
2020-12-07 16:10:51 +0000

    Task-021 complete

---
+++ Task-021/froms3.py
@@ -0,0 +1,14 @@
+import json
+import boto3
+
+## Set auth keys
+with open('/home/n/opt/MindBender_BD/Misc/keys') as keys:
+    s3_keys = json.load(keys)
+    access_key = s3_keys["s3_python_test"]["access_key"]
+    secret_access_key = s3_keys["s3_python_test"]["secret_access_key"]
+
+
+
+s3 = boto3.client('s3', aws_access_key_id=access_key, aws_secret_access_key=secret_access_key)
+s3.download_file('mindbender0001', 'MOCK_DATA.csv', '/home/n/opt/MindBender_BD/Task-021/mock.csv')
+
