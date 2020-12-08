70da07c274ddc9b4cf9c7fe0d9a450baf2addd1e
NathanMinter <nathan.minter@appexperts.me.uk>
2020-12-08 13:52:02 +0000

    Working Hive consumer

---
+++ capstone/consumer.py
@@ -0,0 +1,32 @@
+import json
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+from pyspark.sql import SparkSession
+
+## Create sessions & contexts
+sc = SparkContext("local[*]", "capstone")
+ssc = StreamingContext(sc, 3)
+ss = SparkSession.builder.appName(sc.appName).config("spark.sql.warehouse.dir", "/user/hive/warehouse").config("hive.metastore.urls", "thrift://localhost:9083").enableHiveSupport().getOrCreate()
+
+## Set Kafka stream
+kafkaStream = KafkaUtils.createStream(ssc, "localhost:2181", "capstone", {"capstone": 1})
+
+## Parse messages
+unparsed = kafkaStream.map(lambda x: json.loads(x[1]))
+parsed = unparsed.map(lambda x: (x.get('adult'), x.get('budget'), x.get('genres'), x.get('id'), x.get('original_language'), x.get('popularity'), x.get('production_companies'), x.get('production_countries'), x.get('release_date'), x.get('revenue'), x.get('runtime'), x.get('spoken_languages'), x.get('status'), x.get('title'), x.get('vote_average'), x.get('vote_count')))
+
+## Custom function to process parsed messages
+def process(rdd):
+    if not rdd.isEmpty():
+        global ss
+        df = ss.createDataFrame(rdd, schema=['adult', 'budget', 'genres', 'id', 'original_language', 'popularity', 'production_companies', 'production_countries', 'release_date', 'revenue', 'runtime', 'spoken_languages', 'status', 'title', 'vote_average', 'vote_count'])
+        df.show()
+        df.write.saveAsTable(name="capstone_db.capstone_tbl", format="hive", mode="append")
+
+## Write parsed messages to Hive
+parsed.foreachRDD(process)
+
+ssc.start()
+
+ssc.awaitTermination()
