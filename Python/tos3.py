819cb3810ed47710a724cd1491b6113ca34bfd96
NathanMinter <nathan.minter@appexperts.me.uk>
2020-12-10 18:14:57 +0000

    In-class push to S3 folder excercise

---
+++ Python/tos3.py
@@ -0,0 +1,14 @@
+import json
+import boto3
+import os
+
+## Set auth keys
+with open('/home/n/opt/MindBender_BD/Misc/keys') as keys:
+    s3_keys = json.load(keys)
+    access_key = s3_keys["s3_python_test"]["access_key"]
+    secret_access_key = s3_keys["s3_python_test"]["secret_access_key"]
+
+s3 = boto3.client('s3', aws_access_key_id=access_key, aws_secret_access_key=secret_access_key)
+
+## Upload temp file to S3
+s3.upload_file('/home/n/opt/MindBender_BD/Task-021/mock.csv', 'mindbender0001', 'test/mock.csv')
