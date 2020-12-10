b15a53fad4679738e7ab6cc16173d9f3073a15fd
NathanMinter <nathan.minter@appexperts.me.uk>
2020-12-10 12:55:21 +0000

    Correlation matrix ready

--- capstone/correlation.py
+++ capstone/correlation.py
@@ -4,12 +4,6 @@ import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from pymongo import MongoClient
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import train_test_split
-
-lm = LinearRegression()
 
 ## Instantiate client
 mongo_uri = 'mongodb://localhost:27017'
@@ -27,7 +21,6 @@ print(df)
 ## Create headers for dataframe
 headers = ['_id', 'adult', 'budget', 'genres', 'id', 'original_language', 'popularity', 'production_companies', 'production_countries', 'release_date', 'revenue', 'runtime', 'spoken_languages', 'status', 'title', 'vote_average', 'vote_count']
 used_headers = ['adult', 'budget', 'original_language', 'popularity', 'revenue', 'runtime', 'status', 'vote_average', 'vote_count', 'year', 'month']
-independent_variables = ['adult', 'budget', 'original_language', 'runtime', 'status', 'vote_average', 'vote_count', 'year', 'month']
 df.columns = headers
 
 ## Converts column 'header' to numeric values (to weed out blanks)
@@ -40,6 +33,21 @@ def normalise(header):
 ## Convert categorical variables to dummy variables (0s or 1s)
 def dummy(header):
 	df[header] = pd.get_dummies(df[header])
+
+## Plotting correlation matrix
+def plot_corr_matrix(data,attr,fig_no):
+	correlations = data.corr()
+	fig = plt.figure(fig_no)
+	ax = fig.add_subplot(111)
+	ax.set_xticklabels(attr)
+	ax.set_yticklabels(attr)
+	cax = ax.matshow(correlations,vmax=1,vmin=-1)
+	fig.colorbar(cax)
+	plt.xticks(np.arange(len(attr)))
+	plt.yticks(np.arange(len(attr)))
+	plt.setp(ax.get_xticklabels(), rotation=45, ha="left", rotation_mode="anchor")
+	plt.xlabel('Correlation Matrix')
+	plt.show()
 
 ## Convert release date to categories (month and year)
 df['year'] = df['release_date'].str[:4]
@@ -64,37 +72,6 @@ dummy('status')
 dummy('year')
 dummy('month')
 
-## Dependent variable for linear regression model
-ylm = df['revenue']
-## Independent variables for linear regression model
-xlm = df[['adult', 'budget', 'original_language', 'runtime', 'status', 'vote_average', 'vote_count', 'year', 'month']]
-
-## Fit linear regression model
-ylmrm = lm.fit(xlm, ylm)
-## Prediction model
-ylmhat = lm.predict(xlm)
-
-## Use training set and testing set in 3 folds to give R values for model as out-sample data not in-sample data
-scores = cross_val_score(ylmrm, xlm, ylm, cv=3)
-
-print(scores)
-
-## Get R^2 value (result of 1 is best)
-print(lm.score(xlm,ylm))
-
-## Create train/test variables
-x_train, x_test, y_train, y_test = train_test_split(xlm, ylm, test_size=0.3, random_state=0)
-## Check R^2 values for each polynomial regression to determine the best fitting model (highest R^2 value)
-r2tests = {}
-for x in independent_variables:
-	for n in range(1, 5):
-		pr = PolynomialFeatures(degree=n)
-		x_train_pr = pr.fit_transform(x_train[[x]])
-		x_test_pr = pr.fit_transform(x_test[[x]])
-		lm.fit(x_train_pr, y_train)
-		r2tests["r-{0}-{1}".format(x, n)] = lm.score(x_test_pr, y_test)
-
-print(r2tests)
-
-## Select keys from r2tests with R values above 0.95 (if any)
-
+## Plot correlation heatmap
+data_basic = df.loc[:, used_headers]
+plot_corr_matrix(data_basic, used_headers, 3)
--- capstone/producer.py
+++ capstone/producer.py
@@ -16,27 +16,28 @@ producer = kf.SimpleProducer(kafka)
 
 ## Find popular movies for the day
 movie = tm.Movie()
-popular = movie.popular()
+for page in range(1, 1000):
+    results = movie.popular(page=page)
 
-## Create dict of the relevant information
-for p in popular:
-    movie_data = {}
-    m = movie.details(p.id)
-    movie_data['adult'] = m.adult
-    movie_data['budget'] = m.budget
-    movie_data['genres'] = m.genres #dict
-    movie_data['id'] = m.id
-    movie_data['original_language'] = m.original_language
-    movie_data['popularity'] = m.popularity
-    movie_data['production_companies'] = m.production_companies #dict
-    movie_data['production_countries'] = m.production_countries #dict
-    movie_data['release_date'] = m.release_date
-    movie_data['revenue'] = m.revenue
-    movie_data['runtime'] = m.runtime
-    movie_data['spoken_languages'] = m.spoken_languages #dict
-    movie_data['status'] = m.status
-    movie_data['title'] = m.title
-    movie_data['vote_average'] = m.vote_average
-    movie_data['vote_count'] = m.vote_count
-    ## Send data to Kafka brokers as json
-    producer.send_messages("capstone", bytes(json.dumps(movie_data), 'utf-8'))
+    ## Create dict of the relevant information
+    for p in results:
+        movie_data = {}
+        m = movie.details(p.id)
+        movie_data['adult'] = m.adult
+        movie_data['budget'] = m.budget
+        movie_data['genres'] = m.genres #dict
+        movie_data['id'] = m.id
+        movie_data['original_language'] = m.original_language
+        movie_data['popularity'] = m.popularity
+        movie_data['production_companies'] = m.production_companies #dict
+        movie_data['production_countries'] = m.production_countries #dict
+        movie_data['release_date'] = m.release_date
+        movie_data['revenue'] = m.revenue
+        movie_data['runtime'] = m.runtime
+        movie_data['spoken_languages'] = m.spoken_languages #dict
+        movie_data['status'] = m.status
+        movie_data['title'] = m.title
+        movie_data['vote_average'] = m.vote_average
+        movie_data['vote_count'] = m.vote_count
+        ## Send data to Kafka brokers as json
+        producer.send_messages("capstone", bytes(json.dumps(movie_data), 'utf-8'))
