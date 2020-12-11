a78ac9c9e772d1b25ab49d6f3950516960fbdc49
NathanMinter <nathan.minter@appexperts.me.uk>
2020-12-11 14:16:04 +0000

    Completed in-class example

--- Python/tos3.py
+++ Python/tos3.py
@@ -1,6 +1,7 @@
 import json
 import boto3
 import os
+from botocore.config import Config
 
 ## Set auth keys
 with open('/home/n/opt/MindBender_BD/Misc/keys') as keys:
@@ -10,5 +11,10 @@ with open('/home/n/opt/MindBender_BD/Misc/keys') as keys:
 
 s3 = boto3.client('s3', aws_access_key_id=access_key, aws_secret_access_key=secret_access_key)
 
-## Upload temp file to S3
-s3.upload_file('/home/n/opt/MindBender_BD/Task-021/mock.csv', 'mindbender0001', 'test/mock.csv')
+## Create new bucket
+bucket_name = 'bucket-eit-nathan001'
+location = {'LocationConstraint': 'us-east-2'}
+s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=location)
+
+## Upload file to S3
+s3.upload_file('/home/n/opt/MindBender_BD/Task-021/mock.csv', bucket_name, 'bigdata-eit02/mock.csv')
