fcfd5f0667b8cf99b8525f7a8ae2aff2b91981ea
NathanMinter <nathan.minter@appexperts.me.uk>
2021-01-11 12:01:25 +0000

    Kafka join first commit

---
+++ task-kafka-joins/outer.scala
@@ -0,0 +1,53 @@
+package joining
+
+import java.time.Duration
+import java.util.Properties
+import org.apache.kafka.streams.kstream.JoinWindows
+import org.apache.kafka.streams.scala.ImplicitConversions._
+import org.apache.kafka.streams.scala._
+import org.apache.kafka.streams.scala.kstream._
+import org.apache.kafka.streams.{KafkaStreams, Topology}
+import utils.Settings
+
+class OuterJoinTopology extends App {
+
+  import Serdes._
+
+  // Joined stream properties
+  val props: Properties = Settings.createBasicStreamProperties("outer-join-application", "localhost:9092")
+
+  run()
+
+  private def run(): Unit = {
+    val topology = createTopology()
+    val streams: KafkaStreams = new KafkaStreams(topology, props)
+    streams.start()
+    sys.ShutdownHookThread {
+      streams.close(Duration.ofSeconds(10))
+    }
+  }
+
+  def createTopology(): Topology = {
+    val builder: StreamsBuilder = new StreamsBuilder
+    val left: KStream[Int, String] =
+      builder.stream[Int, String]("LeftTopic")
+    val right: KStream[Int, String] =
+      builder.stream[Int, String]("RightTopic")
+
+    // Do the join
+    val joinedStream = left.outerJoin(right)((l: String, r: String) => {
+      val result = s"Left='${l}', Right='${r}'"
+      result
+    } , JoinWindows.of(Duration.ofSeconds(2)))
+
+    val peeked = joinedStream.peek((k,v)=> {
+
+      val theK = k
+      val theV = v
+    })
+
+    peeked.to("outerJoinOutput")
+
+    builder.build()
+  }
+}
